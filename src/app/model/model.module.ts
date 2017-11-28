38bd103142e99d90494709bf156dfb6abc6fbf32
Jiří Lebduška <jlebduska@scia-online.com>
2017-11-28 16:56:31 +0100

    CHanges

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -8,14 +8,9 @@ import { RestDataSource, REST_URL, REST_URL_DEP } from "./rest.datasource";
     imports: [HttpModule, JsonpModule],
     providers: [ModelRepository,
         { provide: IModelDataSource, useClass: RestDataSource },
-<<<<<<< HEAD
-        { provide: REST_URL, useValue: `http://portal.scia.cz/portalws/dependencyhell/api/SCIADLLs` },
-        { provide: REST_URL_DEP, useValue: `http://${location.hostname}:3500/sciaDllDeps` }]
-=======
         //{ provide: REST_URL, useValue: `http://${location.hostname}:3500/sciaDlls` },
       //{ provide: REST_URL_DEP, useValue: `http://${location.hostname}:3500/sciaDllDeps` }
         { provide: REST_URL, useValue: `http://portal.scia.cz/portalws/dependencyhell/api/SCIADLLs` },
         { provide: REST_URL_DEP, useValue: `http://portal.scia.cz/portalws/dependencyhell/api/DllDependencies` }]
->>>>>>> 3b77d54a23a455ef3bf3bc7bab9a1a5e1511ebe3
 })
 export class ModelModule { }
