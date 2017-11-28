70e80b20ffbb1ebb8e30e8c884fc54911e357460
Jiří Lebduška <jlebduska@scia-online.com>
2017-11-28 21:57:04 +0100

    setting

--- src/app/core/setting.component.ts
+++ src/app/core/setting.component.ts
@@ -6,24 +6,40 @@ import { Observer } from "rxjs/Observer";
 import { Router } from "@angular/router";
 import { DllCategory, DllStatus } from "../model/shared.model";
 
+export class CategorySet {
+    constructor(
+        public n: boolean,
+        public name: string,
+        cat: DllCategory) {   
+    }
+}
+
 @Component({
     selector: 'app-set',
     templateUrl: "setting.component.html"
 })
 export class SettingComponent {
-    count = 0;
-    dllCat = DllCategory;
-    categories() : Array<string> {
-        var keys = Object.keys(this.dllCat);
-        return keys.slice(keys.length / 2);
-    }
-    dllStatus = DllStatus;
-    statuses() : Array<string> {
-        var keys = Object.keys(this.dllStatus);
-        return keys.slice(keys.length / 2);
-    }
+    catset = new Array<CategorySet>();
+
+    // dllCat = DllCategory;
+    // categories() : Array<string> {
+    //     var keys = Object.keys(this.dllCat);
+    //     return keys.slice(keys.length / 2);
+    // }
+    // dllStatus = DllStatus;
+    // statuses() : Array<string> {
+    //     var keys = Object.keys(this.dllStatus);
+    //     return keys.slice(keys.length / 2);
+    // }
     constructor(
         private router: Router,
         private modelRepo: ModelRepository,
-        @Inject(SHARED_STATE) private observer: Observer<SharedState>) { }
+        @Inject(SHARED_STATE) private observer: Observer<SharedState>) {
+        this.catset.push(new CategorySet (false, "Checks", DllCategory.Checks));
+        this.catset.push(new CategorySet (false, "DataModel", DllCategory.DataModel));
+        this.catset.push(new CategorySet (false, "GUI", DllCategory.GUI));
+        this.catset.push(new CategorySet (false, "Kernel", DllCategory.Kernel));
+        this.catset.push(new CategorySet (false, "Storage", DllCategory.Storage));
+        this.catset.push(new CategorySet (false, "UT", DllCategory.UT));
+    }
 }
