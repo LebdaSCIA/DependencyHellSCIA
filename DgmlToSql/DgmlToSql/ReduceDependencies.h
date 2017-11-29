72a7d11cbe3286d65450555d579c9cebeae18beb
Michal Brozek <mbrozek@users.noreply.github.com>
2017-11-29 10:01:13 +0100

    DgmToSql

--- DgmlToSql/DgmlToSql/ReduceDependencies.h
+++ DgmlToSql/DgmlToSql/ReduceDependencies.h
@@ -8,7 +8,7 @@ public:
 	void ModifyDependenciesWithFlags(vector<link>& links);
 
 protected:
-	vecLinkswF GetDependenciesForTarget(short target, const vector<link>& links);
+	info_dll GetDependenciesForTarget(short target, const vector<link>& links);
 	mapTargetToLinkswF GetMapTargetToLinks(const vector<link>& links);
 	vecLinkswF FilterDepFlags(short id, vecLinkswF v);
 	bool IsLinkedId(short id, const vecLinkswF& dependLinksActualLevel, const mapTargetToLinkswF& mapTargets);
